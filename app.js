8cfcfca4ec310eb44b402bb455ab3e141ee4d7af
Claudiu Hiticas <hiticas.claudiu@gmail.com>
2019-12-08 23:46:42 +0200

    Server Side - Add and Delete Data

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ const db = require("./db");
 const collection = "todo";
 const app = express();
 
+
+// schema used for data validation for our todo document
+const schema = Joi.object().keys({
+    todo : Joi.string().required()
+});
+
+// parses json data sent to us by the user 
+app.use(bodyParser.json());
+
 // serve static html file to user
 app.get('/', (req, res) =>{
     res.sendFile(path.join(__dirname, 'index.html'))
@@ -39,7 +48,51 @@ app.put('/:id', (req, res) =>{
     })
 });
 
+//create
+app.post('/',(req,res,next)=>{
+    const userInput = req.body;
 
+    Joi.validate(userInput,schema,(err,result)=>{
+        if(err){
+            const error = new Error("Invalid Input");
+            error.status = 400;
+            next(error);
+        }
+        else{
+            db.getDB().collection(collection).insertOne(userInput,(err,result)=>{
+                if(err){
+                    const error = new Error("Failed to insert Todo Document");
+                    error.status = 400;
+                    next(error);
+                }
+                else
+                    res.json({result : result, document : result.ops[0],msg : "Successfully inserted Todo!!!",error : null});
+            });
+        }
+    })    
+});
+
+//delete
+app.delete('/:id',(req,res)=>{
+    const todoID = req.params.id;
+
+    db.getDB().collection(collection).findOneAndDelete({_id : db.getPrimaryKey(todoID)},(err,result)=>{
+        if(err)
+            console.log(err);
+        else
+            res.json(result);
+    });
+});
+
+// Middleware for handling Error
+// Sends Error Response Back to User
+app.use((err,req,res,next)=>{
+    res.status(err.status).json({
+        error : {
+            message : err.message
+        }
+    });
+})
 
 
 db.connect((err)=>{
